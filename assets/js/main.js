3d7215a571cff0c4b118a899bddf9b7425631685
dua2807 <trinhphuongchi1986@gmail.com>
2024-03-05 16:17:18 +0700

    add file

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+/* ================= SWIPE JS ==============*/
+var swiper = new Swiper(".carousel", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
